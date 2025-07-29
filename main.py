65d5d1cc81d50d1b16324332198d8d46410aa76e
Yassin Chamlal <64170166+yassi22@users.noreply.github.com>
2025-07-29 18:33:53 +0200

    add remove function to code. fixed type bug in dic

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ taken_lijst = []
 
 
 def taak_aanmaken():
+    taak_nummer = int(input("Voer een taak nummer in"))
     taak_naam = input("Voer je taak naam in")
     taak_beschrijving = input("Voer een beschrijving in")
     taak_afgerond = False
 
     taak = {
+        "nummer" : taak_nummer,
         "naam" : taak_naam,
         "beschrijving" : taak_beschrijving,
-         "afgerond" : taak_afgerond
+        "afgerond" : taak_afgerond
     }
 
     taken_lijst.append(taak)
@@ -18,15 +20,22 @@ def taak_aanmaken():
 
 def taken_bekijken():
     for taak in taken_lijst:
-        print(taak['naam'])
-        print(taak['beschrijving'])
-        print(taak['afgerond'])
+        print( "Taak nummer", taak['nummer'])
+        print("Taak naam" + " " + taak['naam'])
+        print("Taak beschrijving" + " " + taak['beschrijving'])
+        print("Taak afgerond", taak['afgerond'])
+
+
+def taak_verwijderen():
+    taak_positie_verwijder = int(input("Voer de taak nummer die je wilt verwijderen"))
+
+    taken_lijst.remove(taak_positie_verwijder)
 
 
 def main():
  taak_aanmaken()
  taken_bekijken()
-
+ taak_verwijderen()
 
 if __name__ == '__main__':
     main()
